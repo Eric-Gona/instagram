b7a799b9d45804e603730bdee5193cf4d291d04a
Eric-Gona <eric.gona254@gmail.com>
2022-03-09 09:33:19 +0300

    adds imports in views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Post,Profile,Like
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import Http404,HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from . forms import Registration,UpdateUser,UpdateProfile,MakeCommentForm,postForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
