971dc1e06f187b365c065d5e66f7a8516f1add08
Eric-Gona <eric.gona254@gmail.com>
2022-03-09 09:15:03 +0300

    adds imports in models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 # Create your models here.
