4e9c645fff069d8bef4b3b71ea00cb738e46e8f4
Eric-Gona <eric.gona254@gmail.com>
2022-03-26 10:42:57 +0300

    changes

--- instagram/settings.py
+++ instagram/settings.py
@@ -19,34 +19,23 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 # development
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
-       
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+
+DATABASES = {
+     'default': dj_database_url.config(
+        default="sqlite:///{}".format(
+            os.path.join(BASE_DIR, 'db.sqlite3')
+        )
+    )
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 cloudinary.config( 
   cloud_name = "gallery24", 
@@ -84,7 +73,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'cloudinary',
-    'bootstrap',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import url
-from django.urls import re_path,path
+
+from django.urls import re_path,include,path
 from django.contrib.auth import views as auth_views
 from . import views as app_views
 
